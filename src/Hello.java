b20e03967e49734766220904f8f78f89e7cf27b7
Pilsung Kang <oss.psk@gmail.com>
2020-12-25 03:35:28 +0900

    Change src/Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
   public static void main(String[] args) {
     Hello hello = new Hello();
     System.out.println(hello.toString());
-    
+    System.out.println("Hello World");
   }
 }
